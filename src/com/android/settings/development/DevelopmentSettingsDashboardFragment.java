643af21e189f9301b5a06b5659100972dc61920c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-11 18:44:41 +0530

    Settings: Add fragment for hide developer status apps [2/2]
    Change-Id: I91e6838f5a330342a1b2c60520418f8d88e82f5a

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -86,6 +86,8 @@ import com.android.settingslib.development.DevelopmentSettingsEnabler;
 import com.android.settingslib.development.SystemPropPoker;
 import com.android.settingslib.search.SearchIndexable;
 
+import co.aospa.settings.development.HideDeveloperStatusPreferenceController;
+
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import java.util.ArrayList;
@@ -788,6 +790,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new ForceEnableNotesRolePreferenceController(context));
         controllers.add(new GrammaticalGenderPreferenceController(context));
         controllers.add(new SensitiveContentProtectionPreferenceController(context));
+        controllers.add(new HideDeveloperStatusPreferenceController(context));
 
         return controllers;
     }
